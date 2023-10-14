19cd5cbdd84cccc30697e59e9673a1f968132d36
julicapponi <julicapponi@hotmail.com>
2023-10-13 21:33:03 -0300

    implementacion de detalle por cada pelicula, se generaliza, luego se implementa en profundidad

--- src/app/pages/detalle-pelicula/detalle-pelicula.page.ts
+++ src/app/pages/detalle-pelicula/detalle-pelicula.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ToastController} from "@ionic/angular";
+import {Result} from "../../clases/MovieResult";
+import {ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   selector: 'app-detalle-pelicula',
@@ -7,10 +9,28 @@ import {ToastController} from "@ionic/angular";
   styleUrls: ['./detalle-pelicula.page.scss'],
 })
 export class DetallePeliculaPage implements OnInit {
+  movie!: Result;
 
-  constructor(private toast: ToastController) { }
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private toast: ToastController) {
+    /*try {
+
+      this.activatedRoute.queryParams.subscribe(() => {
+        if (this.router?.getCurrentNavigation()?.extras?.state) {
+          const movie = this.router?.getCurrentNavigation()?.extras?.state?.movie;
+          if (movie) {
+            this.movie = movie;
+          }
+        }
+      });
+    } catch (e) {
+    }
+    
+     */
+  }
+
 
   ngOnInit() {
+
   }
 
   deletePelicula() {
