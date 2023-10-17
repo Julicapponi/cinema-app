f7b77d099b1bd02fbc80ffa895a994034c67292e
julicapponi <julicapponi@hotmail.com>
2023-10-16 21:38:56 -0300

    nombre de user logueado en menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Storage} from "@ionic/storage-angular";
+import * as Constants from "./constants";
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public appPages = [
     { title: 'Movies', url: '/principal', icon: 'videocam' },
   ];
-  constructor() {}
+   userLogged: any;
+  constructor(private storage: Storage) {
+  }
+
+  async ngOnInit() {
+    await this.storage.create();
+  }
+
+  async menuOpened() {
+    this.userLogged = await this.storage.get(Constants.LOGGED_USER);
+    console.log(this.userLogged);
+  }
 }
--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,2 +1,3 @@
 export const KeyUser = 'users';
 export const KeyMovies = 'movies';
+export const LOGGED_USER = 'logued_user';
